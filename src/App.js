e3a08e1162e243b3dba7bf171b78b9a685b6b74b
Ivan Carrasco <alexcct@hotmail.com>
2022-01-20 10:10:33 -0300

    Movie.js component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Title } from './components/Title';
 import { SearchForm } from './components/SearchForm';
+import { Movie } from './components/Movie';
 import './App.css';
 import 'bulma/css/bulma.css'
 
@@ -11,11 +12,18 @@ class App extends Component {
   _handleResults = (results) => {
     this.setState({ results })
   }
-  
+
   _renderResults () {
     const { results } = this.state
     return results.map(movie => {
-      return <p key={movie.imdbID}> {movie.Title} </p>
+      return (
+        <Movie
+        key={movie.imdbID}
+        title={movie.Title}
+        year={movie.Year}
+        poster={movie.Poster}
+         />
+      )
     })
   }
 
